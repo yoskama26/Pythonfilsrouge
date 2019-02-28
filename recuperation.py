5c085d29cf60e8ecdf1730332cb5bed41eb85f52
yoskama26 <robinpeyret26@gmail.com>
2019-02-28 11:46:04 +0100

    ajout du nb de partie joué

--- recuperation.py
+++ recuperation.py
@@ -1,5 +1,6 @@
 import os
 import json
+dico2 = {}
 
 
 def pied():
@@ -7,9 +8,15 @@ def pied():
     for partie in os.listdir('./doliprane1000'):
         with open('./doliprane1000/' + partie, 'r') as fichier:
             stats = json.loads(fichier.read())
-            print(stats)
+#            print(stats)
             table_parties.append(stats)
     return table_parties
 
 
-pied()
+for partie in pied():
+    dateu = partie["Start time"][:8]
+    if dateu in dico2:
+        dico2[dateu] += 1
+    else:
+        dico2[dateu] = 1
+print(dico2)
